0de4e29e711b02e815d8cb468971c0ab1697ae97
Amir Hossein <rahmanzadeh.99@gmail.com>
2021-06-17 19:05:30 +0430

    input logic added

--- main.dart
+++ main.dart
@@ -1,1 +1,16 @@
 //This is a simple game to practice dart
+import 'dart:io';
+
+void main() {
+  while (true) {
+    stdout.write("Rock paper or scissors? (r,p,s) ");
+    final input = stdin.readLineSync();
+    if (input == "r" || input == "p" || input == "s") {
+      print(input);
+    } else if (input == "q") {
+      break;
+    } else {
+      print("Invalid input");
+    }
+  }
+}
